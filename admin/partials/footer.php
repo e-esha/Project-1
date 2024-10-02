bf6c7adac5e1fa3039e967fbfe7fb766b983376a
e-esha <141734766+e-esha@users.noreply.github.com>
2024-10-02 18:15:43 +0545

    Delete admin directory

--- admin/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-    <div class="wrapper">
-    <p class="text-center">
-    &copy; 2024 Dhulikhel Restaurant, Designed By-<a href="#">Esha Tamang.  </a>All rights reserved. 
-</p>
-    </div>
-    </div>
